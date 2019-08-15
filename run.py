d06c5f2553cc1344b609e981330d72d52804c9f0
litian6363 <litian6363@163.com>
2019-08-15 11:12:50 +0800

    modifly for travis

--- run.py
+++ run.py
@@ -3,8 +3,8 @@
 
 __author__ = 'LiTian'
 
-# from gevent import monkey
-# monkey.patch_all()
+from gevent import monkey
+monkey.patch_all()
 
 from web import app
 
@@ -12,10 +12,13 @@ app = app
 app.app_context().push()
 
 if __name__ == '__main__':
-    # from web.tools.cookie_factory import recreate_database_and_admin
-    # recreate_database_and_admin(app, admin_password='123456', delect_table=True)  # 重建数据库和创建管理员
+    ip = '127.0.0.1'
+    port = 5000
+    from web.tools.cookie_factory import recreate_database_and_admin
+    recreate_database_and_admin(app, admin_password='123456', delect_table=True)  # 重建数据库和创建管理员
 
     # 直接运行时，gevent所需，
     from gevent import pywsgi
-    server = pywsgi.WSGIServer(('127.0.0.1', 5000), app)
+    server = pywsgi.WSGIServer((ip, port), app)
+    print('运行web服务：'+ ip + '：' + str(port))
     server.serve_forever()
